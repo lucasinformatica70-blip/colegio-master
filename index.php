ca7b612724b3e3ed4f191059fc2fd1d82da4b011
lucasinformatica70-blip <lucasinformatica70@gmail.com>
2026-06-25 08:30:31 -0300

    applied Bulma.io ig

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
+    <meta author="Lucas Vieira">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css">
 </head>
 <body>
     
